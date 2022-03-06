7183004d7b998d02d75a8d4cb92e3437f553758c
Ascor8522 <vinoa.89.jeux@gmail.com>
2022-03-06 16:27:29 +0100

    Code cleanup.
    Since we are using Node.js 16 and above, we might as well use modern features:
    - turned the main file into a module, so we can use top level await
    - we don't need to use an IIFE to await async code anymore
    - turned variables into proper functions
    - removed un-necessary await everywhere, since everything is now one long and clean promise chain
    - removed the un-necessary try-catch statements, since errors in promises can be caught once at the end of the chain
    - added some checks after fetches for extra safety
    
    Also:
    - Added documentation to functions and some parameter / return types with JSDoc.
    - Created a config object at the top of the file. An .env file could have been used too.
    - Made code more easily understandable, split it into smaller function with more descriptive names.
    - GitPod file no longer needed, just use npx command to run a cli script from installed packages.
    - Added Node.js >=16.0.0 as a requirement to the npm package.
    - Removed un-needed packages (fs-extra) since we were only using 1 function that can be re-written easily.
    - Added tsconfig file so VSCode can automatically check the file with the builtin TypeScript compiler.

---
+++ src/types.mjs
@@ -0,0 +1,23 @@
+/**
+ * This file includes the types used in this project.
+ * Switching to TypeScript would be much better but here we are...
+ */
+
+/**
+ * @typedef {object} GitHubContributor
+ * @property {string} login
+ * @property {number} contributions
+ * @property {string} avatar_url
+ */
+
+/**
+ * @typedef {object} SrcContributors
+ * @property {string} login
+ * @property {string} avatar_url
+ * @property {string} name
+ * @property {string} profile
+ * @property {string[]} contributions
+ */
+
+// TODO: Add the GitHub API types
+// (there might be an existing npm package that does that, but be we only need 2/3 types, so we might as well just write them here)
